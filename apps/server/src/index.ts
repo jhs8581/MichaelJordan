7d5a3e608c7c57b2d62e5809b7deecb4e5d90914
hsjeong <hsjeong@cnspartner.com>
2026-05-29 05:30:20 +0900

    fix: allow patch requests in cors

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -40,7 +40,7 @@ async function main() {
       }
     },
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
